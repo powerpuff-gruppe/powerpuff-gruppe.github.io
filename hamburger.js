e6908764b9a3762ec8a70e4ccb0dbe43d5cf19c7
SofieHedemann <sofhedemann@gmail.com>
2023-05-30 19:13:46 +0200

    opdatering på hamburger

---
+++ hamburger.js
@@ -0,0 +1,25 @@
+// hamburger menu
+// Selecting element in the HTML document:
+const hamburger = document.querySelector('.hamburger');
+console.log(hamburger); // to see what is in the variable hamburger
+const navMenu = document.querySelector('.nav-menu');
+const navLink = document.querySelectorAll('.nav-link') // querySelectorAll vælger alle i samme class
+console.log(navLink)
+
+// Listening for a click event on hamburger button:
+hamburger.addEventListener('click', mobileMenu);
+
+//Event Listener for all navLinks: 
+navLink.forEach(n => n.addEventListener('click', closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
+
+//Dette får menuen til at lukke igen, når man har trykket på et link
+//When a menu-item is clicked - the active classes are removed from the hamburger and navMenu
+function closeMenu() {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}
